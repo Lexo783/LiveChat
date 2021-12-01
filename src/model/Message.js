aed0bcc38e0d79aaf4c8dee5533db99439fad7de
Lexo783 <florentdebuchy@orange.fr>
2021-12-01 09:52:40 +0100

    add room in model message

--- src/model/Message.js
+++ src/model/Message.js
@@ -2,7 +2,8 @@ import database from "mongoose"
 
 const schema = new database.Schema({
     creator: {type: Number},
-    text: {type: String}
+    text: {type: String},
+    room: {type: String},
     //option: timestamps ajoute le created_at et update_at
     //option: minimize ... j'ai pas trop compris mais ça peut faire des erreurs a cause d'une clef name donc TKT GROS ON EST BON
 },{
