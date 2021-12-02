32aa6677c48bf5b730ff9e20948203081a09d835
Marion <meurantmarion@gmail.com>
2021-12-02 15:43:58 +0100

    Add nunjucks block in login, register and home

--- src/controller/registerController.js
+++ src/controller/registerController.js
@@ -1,6 +1,6 @@
 export function registerController(request, response) {
     response.render('register/register.html', {
         title_text: "HERE",
-        title: "here"
+        title: "Register"
     })
 }
